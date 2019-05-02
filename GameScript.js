f58d5d7aa68279e5bae0590285120d552e977c51
Nina <ninawu424@gmail.com>
2019-05-02 18:14:27 -0400

    Nina's first commit

---
+++ GameScript.js
@@ -0,0 +1,40 @@
+﻿var cards = [
+	{topic:"classroom", vocabs: ["desk", "student", "teacher","school", "learn"]},
+	{topic:"christmas", vocabs: ["december", "santa", "tree", "presents", "celebrate"]},
+	{topic:"lighting", vocabs: ["wet", "rain", "thunder", "bolt", "umbrella"]},
+	{topic:"water bottle", vocabs: ["drink", "thirsty", "hot", "sports", "fill"]}
+];
+var index = 0;
+
+function firstCard() {
+	displayCard();
+}
+
+function prevCard() {
+	if (index != 0) {
+		index--;
+		displayCard();
+	}	
+}
+
+function nextCard() {
+	if (index != (cards.length - 1)) {
+		index ++;
+		displayCard();
+	}	
+}
+
+function displayVocabs() {
+	var vocabs = cards[index].vocabs;
+	var txt = "<ul>";
+	for (var i = 0; i < vocabs.length; i++){
+		txt += "<li>" + vocabs[i] + "</li>" + "<br>"; 
+	} 		    
+	return txt;
+}
+
+function displayCard() {
+	document.getElementById("topicsTxt").innerHTML = cards[index].topic.toUpperCase();
+	document.getElementById("vocabsTxt").innerHTML = displayVocabs().toUpperCase();
+}
+		
