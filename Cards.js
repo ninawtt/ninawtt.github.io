14c808395a2549d95a6a12b15ee7bc57fba2962c
Nina <ninawu424@gmail.com>
2019-05-03 13:58:42 -0400

    Add Chinese Version

--- Cards.js
+++ Cards.js
@@ -1,7 +1,15 @@
-var CARDS = [
+var EN_CARDS = [
 	{topic:"classroom", vocabs: ["desk", "student", "teacher","school", "learn"]},
 	{topic:"christmas", vocabs: ["december", "santa", "tree", "presents", "celebrate"]},
 	{topic:"lighting", vocabs: ["wet", "rain", "thunder", "bolt", "umbrella"]},
 	{topic:"water bottle", vocabs: ["drink", "thirsty", "hot", "sports", "fill"]},
 	{topic:"head", vocabs: ["face", "toe", "part", "body", "torso"]}
+];
+
+var CH_CARDS = [
+	{topic:"教室", vocabs: ["書桌", "學生", "老師","學校", "學習"]},
+	{topic:"聖誕節", vocabs: ["十二月", "聖誕老公公", "樹", "禮物", "慶祝"]},
+	{topic:"閃電", vocabs: ["濕", "雨", "雷聲", "閃電", "雨傘"]},
+	{topic:"水壺", vocabs: ["喝", "渴", "熱", "運動", "裝水"]},
+	{topic:"頭", vocabs: ["臉", "腳趾", "部位", "身體", "軀幹"]}
 ];
--- GameScript.js
+++ GameScript.js
@@ -1,26 +1,47 @@
 ﻿
-var index = 0;
+var indexOfCards = 0;
+var langEn = true;
+var cards = EN_CARDS;
+
+function changeToEn() {
+	langEn = true, cards = EN_CARDS;
+	firstCard();
+}
+
+function changeToCh() {
+	langEn = false, cards = CH_CARDS;
+	firstCard();
+}
 
 function firstCard() {
+	indexOfCards = 0;
 	displayCard();
 }
 
 function prevCard() {
-	if (index != 0) {
-		index--;
+	if (indexOfCards != 0) {
+		indexOfCards--;
+		displayCard();
+	}
+	else {
+		indexOfCards = cards.length - 1;
 		displayCard();
 	}	
 }
 
 function nextCard() {
-	if (index != (CARDS.length - 1)) {
-		index ++;
+	if (indexOfCards != (cards.length - 1)) {
+		indexOfCards ++;
 		displayCard();
 	}	
+	else {
+		indexOfCards = 0;
+		displayCard();
+	}
 }
 
 function displayVocabs() {
-	var vocabs = CARDS[index].vocabs;
+	var vocabs = cards[indexOfCards].vocabs;
 	var txt = "<ul>";
 	for (var i = 0; i < vocabs.length; i++){
 		txt += "<li>" + vocabs[i] + "</li>" + "<br>"; 
@@ -29,7 +50,7 @@ function displayVocabs() {
 }
 
 function displayCard() {
-	document.getElementById("topicsTxt").innerHTML = CARDS[index].topic.toUpperCase();
+	document.getElementById("topicsTxt").innerHTML = cards[indexOfCards].topic.toUpperCase();
 	document.getElementById("vocabsTxt").innerHTML = displayVocabs().toUpperCase();
 }
 		
